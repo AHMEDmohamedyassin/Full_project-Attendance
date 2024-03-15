450051123c965ea7c6aec5d351b1828c08d7a795
AHMEDmohamedyassin <ahmedmohamed982025@outlook.com>
2024-03-16 00:01:33 +0200

    v5

--- web/src/Pages/Student/ReadQrPage.js
+++ web/src/Pages/Student/ReadQrPage.js
@@ -6,22 +6,24 @@ const ReadQrPage = () => {
     const [data, setData] = useState('No result');
 
   return (
-    <>
-      <QrReader
-        onResult={(result, error) => {
-          if (!!result) {
-            setData(result?.text);
-          }
+    <div className='contain'>
+      <div className='font-bold'>{data}</div>
+      <div className='rounded-lg overflow-hidden border-gray-200 border-[1px] py-0'>
+        <QrReader
+          onResult={(result, error) => {
+            if (!!result) {
+              setData(result?.text);
+            }
 
-          if (!!error) {
-            console.info(error);
-            setData('error')
-          }
-        }}
-        style={{ width: '100%' }}
-      />
-      <p>{data}</p>
-    </>
+            if (!!error) {
+              console.info(error);
+              setData('error')
+            }
+          }}
+          scanDelay={1000}
+        />
+      </div>
+    </div>
   )
 }
 
