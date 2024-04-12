37a5f0acf20e0c2d5e9fca433ba741ddb8c1df85
medicalLab1 <TheMedicalLab1@gmail.com>
2024-04-12 18:21:27 +0200

    v5

--- web/src/Pages/AboutUsPage.js
+++ web/src/Pages/AboutUsPage.js
@@ -1,16 +1,39 @@
 import React from 'react'
+import Ahmed from '../assets/person/Ahmed.jpg'
 
 const AboutUsPage = () => {
   return (
     <div className='contain my-10 leading-10 text-gray-500' style={{wordSpacing:'4px'}}>
-      <div className=' text-justify'>هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل الخارجي للنص أو شكل توضع الفقرات في الصفحة التي يقرأها. ولذلك يتم استخدام طريقة لوريم إيبسوم لأنها تعطي توزيعاَ طبيعياَ -إلى حد ما- للأحرف عوضاً عن استخدام "هنا يوجد محتوى نصي، هنا يوجد محتوى نصي" فتجعلها تبدو (أي الأحرف) وكأنها نص مقروء. العديد من برامح النشر المكتبي وبرامح تحرير صفحات الويب تستخدم لوريم إيبسوم بشكل إفتراضي كنموذج عن النص، وإذا قمت بإدخال "lorem ipsum" في أي محرك بحث ستظهر العديد من المواقع الحديثة العهد في نتائج البحث. على مدى السنين ظهرت نسخ جديدة ومختلفة من نص لوريم إيبسوم، أحياناً عن طريق الصدفة، وأحياناً عن عمد كإدخال بعض العبارات الفكاهية إليها. </div>
+      <div className=' text-justify'>
+
+        <p className='font-bold'>اننا بنهدف لحل مشكلة الغياب في اكثر من نقطه :</p>
+        <ul>
+          <li>قدم الطريقه التقليديه الورقيه <span className="material-symbols-outlined mx-1">note_stack</span> لاخذ الحضور مع وجود طرق احدث .</li>
+          <li>معاناه الدكاتره في تفريغ الاسماء من ورق <span className="material-symbols-outlined mx-1">note_stack</span> الحضور .</li>
+          <li>عدم اتنزاف وقت <span className="material-symbols-outlined mx-1">schedule</span> المحاضرات في اخذ الحضور .</li>
+        </ul>
+
+        <p className='font-bold mt-4'>ولذلك قمنا بتوفير طرق متعدد لأخذ الحضور : </p>
+        <ul>
+          <li> استخدام الرمز <span className="material-symbols-outlined mx-1">qr_code_2</span> حتي يمكن للطلبة التقات الرمز<span className="material-symbols-outlined mx-1">qr_code_2</span>  الخاص بالمحاضرة .</li>
+          <li>استخدام الكاميرا <span className="material-symbols-outlined mx-1">photo_camera</span> لالتقات الرمز <span className="material-symbols-outlined mx-1">qr_code_2</span> الخاص بالطالب .</li>
+          <li>ادخال كود الطالب بشكل يدوي <span className="material-symbols-outlined mx-1">keyboard</span> .</li>
+        </ul>
+        
+        <p className='font-bold mt-4'>يوفر التطبيق للمحاضر تحويل كل بيانات الطلبة من الأسماء و رقم الفصل و رقم المجموعة و الرقم بالفصل إلي ملف إكسل EXCEL لسهول تسجيل الحضور علي المحاضر</p>
+      </div>
 
       <div className='mt-10 pt-10 border-gray-200 border-t-[1px]'>
         <h1 className='text-lg font-bold text-gray-500'>تحت إدارة :</h1>
         <div className='flex flex-wrap lg:justify-between justify-center items-center gap-10 mt-10'>
           {/* card */}
           <div className='relative w-80 h-96 shadow-mainShadow shadow-lg rounded overflow-hidden'>
-            <img className='w-full h-full' src='https://images.pexels.com/photos/2470655/pexels-photo-2470655.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'/>
+            <div style={{
+              backgroundImage:`url('${Ahmed}')`,
+              backgroundRepeat:'no-repeat' ,
+              backgroundPosition:'top' ,
+              backgroundSize:'cover'
+            }} className='w-full h-full'></div>
             <h1 className='absolute bottom-0 left-0 w-full text-lg text-gray-500 font-bold p-4 py-2 bg-mainWhite/50 shadow-mainShadow'>أحمد محمد ياسين</h1>
           </div>
 
--- web/src/Pages/ContactUsPage.js
+++ web/src/Pages/ContactUsPage.js
@@ -7,19 +7,12 @@ const ContactUsPage = () => {
             {/* email */}
             <div className='flex sm:flex-nowrap flex-wrap items-center gap-4 border-b-[1px] border-gray-200 py-10'>
                 <h1 className='text-lg font-bold text-gray-700 whitespace-nowrap'>البريد الإليكتروني / </h1>
-                <a className='text-mainblue sm:text-xl text-lg hover:text-mainBlue' style={{wordWrap:'anywhere'}} href='mailto:ahmedmohamed@outlook.com'>ahmedmohamed@outlook.com</a>
+                <a className='text-mainblue sm:text-xl text-lg hover:text-mainBlue' style={{wordWrap:'anywhere'}} href='mailto:ahmedmohamed982025@outlook.com'>ahmedmohamed@outlook.com</a>
             </div>
             {/* mobile phone */}
             <div className='flex sm:flex-nowrap flex-wrap gap-4 border-b-[1px] border-gray-200 py-10'>
                 <h1 className='text-lg font-bold text-gray-700 whitespace-nowrap'>أرقام الهاتف / </h1>
                 <div className='flex items-center justify-start flex-wrap gap-2'>
-                    <a className='text-mainblue text-lg hover:text-mainBlue' href='tel:01066404523'>01066404523</a>
-                    <a className='text-mainblue text-lg hover:text-mainBlue' href='tel:01066404523'>01066404523</a>
-                    <a className='text-mainblue text-lg hover:text-mainBlue' href='tel:01066404523'>01066404523</a>
-                    <a className='text-mainblue text-lg hover:text-mainBlue' href='tel:01066404523'>01066404523</a>
-                    <a className='text-mainblue text-lg hover:text-mainBlue' href='tel:01066404523'>01066404523</a>
-                    <a className='text-mainblue text-lg hover:text-mainBlue' href='tel:01066404523'>01066404523</a>
-                    <a className='text-mainblue text-lg hover:text-mainBlue' href='tel:01066404523'>01066404523</a>
                     <a className='text-mainblue text-lg hover:text-mainBlue' href='tel:01066404523'>01066404523</a>
                 </div>
             </div>
@@ -27,13 +20,6 @@ const ContactUsPage = () => {
             <div className='flex sm:flex-nowrap flex-wrap gap-4 border-b-[1px] border-gray-200 py-10'>
                 <h1 className='text-lg font-bold text-gray-700 whitespace-nowrap'>أرقام الواتساب / </h1>
                 <div className='flex items-center justify-start flex-wrap gap-2'>
-                    <a className='text-mainblue text-lg hover:text-mainBlue' href='whatsapp://send?phone=+201066404523'>01066404523</a>
-                    <a className='text-mainblue text-lg hover:text-mainBlue' href='whatsapp://send?phone=+201066404523'>01066404523</a>
-                    <a className='text-mainblue text-lg hover:text-mainBlue' href='whatsapp://send?phone=+201066404523'>01066404523</a>
-                    <a className='text-mainblue text-lg hover:text-mainBlue' href='whatsapp://send?phone=+201066404523'>01066404523</a>
-                    <a className='text-mainblue text-lg hover:text-mainBlue' href='whatsapp://send?phone=+201066404523'>01066404523</a>
-                    <a className='text-mainblue text-lg hover:text-mainBlue' href='whatsapp://send?phone=+201066404523'>01066404523</a>
-                    <a className='text-mainblue text-lg hover:text-mainBlue' href='whatsapp://send?phone=+201066404523'>01066404523</a>
                     <a className='text-mainblue text-lg hover:text-mainBlue' href='whatsapp://send?phone=+201066404523'>01066404523</a>
                 </div>
             </div>
